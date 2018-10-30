4109cdd1e0f4078f0dd93115b20b4f16260eaada
Sachin <sachin_rcz@yahoo.co.in>
2018-10-30 16:42:15 +0530

    stack and queues

---
+++ src/Client.java
@@ -0,0 +1,39 @@
+
+public class Client {
+	
+	static void stacktest() {
+		Stack<Integer> s = new Stack<Integer>();
+		s.push(1);
+		s.push(2);
+		s.pop();
+		s.push(3);
+		s.pop();
+		s.pop();
+		for(Integer i: s) {
+			System.out.println(i);
+		}
+		
+	}
+	
+	static void queuetest() {
+		Queue<String> q = new Queue<String>();
+		q.enqueue("l");
+		q.enqueue("a");
+		q.enqueue("m");
+		q.dequeue();
+		q.dequeue();
+		for(String s:q) {
+			System.out.println(s);
+		}
+	}
+	
+	public static void main(String[] args) {
+		Bag<Double> bag = new Bag<Double>();
+		bag.add(0.0);
+		bag.add(1.2);
+		for(Double b:bag) {
+			System.out.println(b);
+		}
+	}
+
+}
---
+++ src/QueueOfStrings.java
@@ -0,0 +1,36 @@
+
+public class QueueOfStrings {
+	
+	private class Node{
+		String item;
+		Node next;
+	}
+	
+	private Node first;
+	private Node last;
+	
+	
+	public String dequeue() {
+		String item = first.item;
+		first = first.next;
+		if (isEmpty()) last = null;
+		return item;
+	}
+	
+	public void enqueue(String item) {
+		Node temp = last;
+		last = new Node();
+		last.item = item;
+		last.next = null;
+		if(isEmpty()){
+			first = last;
+		}else {
+			temp.next = last;
+		}
+	}
+	
+	public boolean isEmpty() {
+		return first==null;
+	}
+
+}
